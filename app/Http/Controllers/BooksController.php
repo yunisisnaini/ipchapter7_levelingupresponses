697132610cca565e7da9d309050e1511c87a367b
Yunis Isnaini <yunisyou30@gmail.com>
2024-04-29 23:17:41 +0700

    first commit

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use App\Transformer\BookTransformer;
+
+class BooksController extends Controller
+{
+    /**
+     * GET /books
+     * @return array
+     */
+    public function index()
+    {
+        return $this->collection(Book::all(), new BookTransformer());
+    }
+
+    public function show($id)
+    {
+        return $this->item(Book::findOrFail($id), new BookTransformer());
+    }
+
+    public function store(Request $request)
+    {
+        $book = Book::create($request->all());
+        $data = $this->item($book, new BookTransformer());
+
+        return response()->json($data, 201, [
+            'Location' => route('books.show', ['id' => $book->id])
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $book = Book::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => [
+                    'message' => 'Book not found'
+                ]
+            ], 404);
+        }
+
+        $book->fill($request->all());
+        $book->save();
+
+        return $this->item($book, new BookTransformer());
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $book = Book::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => [
+                    'message' => 'Book not found'
+                ]
+            ], 404);
+        }
+
+        $book->delete();
+        return response(null, 204);
+    }
+
+    public function destroyall()
+    {
+        $book = Book::truncate();
+        // $book->delete();x
+        return response(null, 204);
+    }
+}
